78233aa00b2ca4bf03415184cb19d90e79f50401
patorma <pocontreras@ing.ucsc.cl>
2024-06-14 01:39:48 -0400

    se agrego método de actualizar valorpara cambiar el valor total a 0 en caso de eliminar todos los registros

--- src/app/gastos/pages/by-gasto/by-gasto.component.ts
+++ src/app/gastos/pages/by-gasto/by-gasto.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GastosService } from '../../gastos.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { DetalleGastoDTO } from '../../interfaces/detalle-gasto.interface';
 import { Gasto } from '../../interfaces/gasto.interface';
 import Swal from 'sweetalert2';
@@ -8,102 +8,98 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'by-gasto',
   templateUrl: './by-gasto.component.html',
-  styleUrls:['./by-gasto.component.css']
-
+  styleUrls: ['./by-gasto.component.css']
 })
 export class ByGastoComponent implements OnInit {
 
-  public detalleGastoDTO : DetalleGastoDTO [] = []
+  public gastoid: number =0;
+  public detalleGastoDTO: DetalleGastoDTO[] = [];
   public valor: number = 0;
-  gasto: Gasto = {id: 0, // Supongamos que el id predeterminado es 0
-  descripcion: '', // La descripción puede inicializarse como una cadena vacía
-  fechaGasto: '', // La fecha puede inicializarse como una cadena vacía también
-  tipoGasto: 'OTRO'};
+  public gasto: Gasto = { id: 0, descripcion: '', fechaGasto: '', tipoGasto: 'OTRO' };
+  public title: string = 'Detalle del gasto';
+  public nombreGasto: string ='';
 
-  public title: string = 'Detalle del gasto'
-  constructor(private gastoService: GastosService,private router: Router,
-    private activatedRoute:ActivatedRoute){}
+  constructor(private gastoService: GastosService, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.cargarDetalleGasto(),
-    this.activatedRoute.params.subscribe((params)=>{
-      let id =params['id']
-      if(id){
-        this.gastoService.subTotalGastosById(id)
-               .subscribe((total)=> this.valor = total )
+    this.activatedRoute.params.subscribe((params) => {
+      this.gastoid = +params['id'];// se usa el + para transformar params['id'] de un string a number
+    //  console.log( typeof(this.gastoid))
+      if (this.gastoid) {
+        this.cargarDetalleGasto();
+        this.actualizarValorTotal();
       }
-    })
-
+    });
   }
 
- cargarDetalleGasto(): void{
-  this.activatedRoute.params.subscribe((params)=>{
-    let id =params['id']
-    if(id){
-      this.gastoService
-         .obtenerDetallesGastos(id)
-         .subscribe((detalle)=>this.detalleGastoDTO = detalle)
-    }
-  }),
-  this.verGasto()
- }
+  cargarDetalleGasto(): void {
+    this.gastoService.obtenerDetallesGastos(this.gastoid).subscribe((detalle) => {
+      this.detalleGastoDTO = detalle;
+    });
+    this.verGasto();
+  }
 
- verGasto():void{
-  this.activatedRoute.params.subscribe((params)=>{
-    let id =params['id']
-    if(id){
-      this.gastoService
-         .getGastosById(id)
-         .subscribe((gasto) =>this.gasto = gasto)
-    }
-  })
- }
+  verGasto(): void {// implementar en el html ver el gasto que se le asigana el detalle
+    this.gastoService.getGastosById(this.gastoid).subscribe((gasto) => {
+      this.gasto = gasto;
+      console.log(this.gasto)
+      this.nombreGasto = this.gasto.descripcion
+    });
+  }
 
- deleteDetalleGasto(detalle: DetalleGastoDTO): void{
-  const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger',
-    },
-    buttonsStyling: false,
-  });
-  swalWithBootstrapButtons
-  .fire({
-    title: 'Está seguro?',
-        text: `¿Seguro que desea elimina ${detalle.producto} del detalle de gasto?`,
+  actualizarValorTotal(): void {
+    this.gastoService.subTotalGastosById(this.gastoid).subscribe({
+      next: (total) => {
+        this.valor = total;
+      },
+      error: (err) => {
+        console.error('Error al obtener el subtotal:', err);
+        this.valor = 0; // Asigna 0 o cualquier valor predeterminado en caso de error
+      }
+    });
+}
+
+  deleteDetalleGasto(detalle: DetalleGastoDTO): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger',
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: '¿Está seguro?',
+        text: `¿Seguro que desea eliminar ${detalle.producto} del detalle de gasto?`,
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonText: 'Si, eliminar!',
+        confirmButtonText: 'Sí, eliminar!',
         cancelButtonText: 'No, cancelar!',
         reverseButtons: true,
-  })
-  .then((result)=>{
-    if (result.value) {
-      this.gastoService.deleteDetalleGasto(detalle.id).subscribe({
-        next: ()=>{
-          this.detalleGastoDTO = this.detalleGastoDTO.filter((dt)=> dt != detalle);
-          this.router.navigate(['/gastos']);
-          swalWithBootstrapButtons.fire(
-            'Detalle del gasto elimando!!',
-            `El producto ${detalle.producto} fue eliminado con éxito`,
-            'success'
-          )
-        },
-        error: (err) =>{
-              // Aquí manejas el error
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.gastoService.deleteDetalleGasto(detalle.id).subscribe({
+            next: () => {
+              this.detalleGastoDTO = this.detalleGastoDTO.filter((dt) => dt !== detalle);
+              swalWithBootstrapButtons.fire(
+                'Detalle del gasto eliminado!!',
+                `El producto ${detalle.producto} fue eliminado con éxito`,
+                'success'
+              );
+              // Actualizar el valor total después de eliminar un detalle
+              this.actualizarValorTotal();
+            },
+            error: (err) => {
               if (err.error.mensaje) {
-                // Muestra el mensaje de error usando Swal
                 swalWithBootstrapButtons.fire('Error', err.error.mensaje, 'error');
               } else {
-                // Si no hay mensaje de error personalizado, muestra un mensaje genérico
                 swalWithBootstrapButtons.fire('Error', 'Ha ocurrido un error al eliminar el producto del detalle de gasto.', 'error');
               }
-
+            }
+          });
         }
-      })
-    }
-  })
- }
-
-
+      });
+  }
 }
